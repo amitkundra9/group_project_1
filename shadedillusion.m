e3f0193541acab3d9cb45f8dfc6c85025fc8b81f
amitkundra <amitkundra@amits-mbp.devices.brown.edu>
2021-03-07 11:35:39 -0500

    Finalized image and dimensions, put into function form to be called by PTB, and added options to display image in different colors

--- shadedillusion.m
+++ shadedillusion.m
@@ -1,15 +1,30 @@
-img = repmat(0:255,256,1);
-for ii = 42:86
-    for jj = 96:150
+function shadedillusion(color_map)
+
+img = repmat(0:255,256,1); %creates baseline matrix with squares at appropriate posistions
+
+%creates little square on left sisde
+for ii = 48:80
+    for jj = 112:144
         img(jj,ii) = 128;
     end
 end
-for kk = 170:214
-    for ll = 96:150
+
+%creates little square on right side
+for kk = 176:208
+    for ll = 112:144
         img(ll,kk) = 128;
     end
 end
-colormap gray
-axis image;
-axis off;
+
+%sets colormap depending on user input
+if color_map ==  1
+    colormap gray
+elseif color_map == 2
+    colormap parula 
+elseif color_map == 3
+    colormap cool
+end
+
+%displays image
 imagesc(img)
+axis image; axis off;
